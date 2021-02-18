33e49c9b3e4783d25f179cd269550408a1c9040c
jeremyrrose <jeremy.r.rose@gmail.com>
2021-02-17 23:28:02 -0500

    fixed five_inserter, added alt five_inserter

--- index.py
+++ index.py
@@ -9,9 +9,10 @@ def five_inserter (original):
     try:
         insert_here = list(map(lambda n: n <= 5, digits)).index(True)
     except:
-        insert_here = -1
+        insert_here = len(digits)
 
-    digits = digits[0:insert_here] + [5] + digits[insert_here:-1]
+    # insert 5 at specified index
+    digits = digits[:insert_here] + [5] + digits[insert_here:]
 
     # return an integer derived from the joined digits
     return int(''.join(str(n) for n in digits))
@@ -19,3 +20,27 @@ def five_inserter (original):
 print(five_inserter(67891237043599))
 print(five_inserter(12341234))
 print(five_inserter(99996786666))
+
+
+def five_inserter_two (original):
+    
+    found = None
+    string = ''
+
+    # loop through an ordered array of the digits
+    # add a 5 and change found if n <= 5
+    for n in str(original):
+        if not found and int(n) <= 5:
+            string += f'5{n}'
+            found = True
+        else:
+            string += n
+    
+    if not found:
+        string += '5'
+
+    return int(string)
+
+print(five_inserter_two(67891237043599))
+print(five_inserter_two(12341234))
+print(five_inserter_two(99996786666))
